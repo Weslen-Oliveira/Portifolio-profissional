ee14f3c5c29e009eafc400f01adce2af74ccd51e
Weslen-Oliveira <weslenoliveira710@gmail.com>
2025-10-13 19:47:56 -0300

    animação no scroll

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ menuCelular.addEventListener('click', function(){
           }
           
 })
+function iniciarAnimacaoScroll(){
+    const sections = document.querySelectorAll('.js-scroll');   
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.5;  
+        function animaScroll(){
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const iniciarScrollSuave = (sectionTop - windowMetade) < 0;
+                if(iniciarScrollSuave){
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            }); 
+        }
+        animaScroll();
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+iniciarAnimacaoScroll();
